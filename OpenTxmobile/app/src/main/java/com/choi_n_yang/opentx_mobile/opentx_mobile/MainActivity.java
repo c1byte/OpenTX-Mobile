03e0c07b35b78fb9e6f000d1ea07951ed2bc7b3f
c1byte <kyuhae.choi@gmail.com>
2018-07-08 12:20:54 +0900

    update Usb serial data

--- OpenTxmobile/app/src/main/java/com/choi_n_yang/opentx_mobile/opentx_mobile/MainActivity.java
+++ OpenTxmobile/app/src/main/java/com/choi_n_yang/opentx_mobile/opentx_mobile/MainActivity.java
@@ -1,7 +1,9 @@
 package com.choi_n_yang.opentx_mobile.opentx_mobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void  onClick(View view) {
+        Intent intent = new Intent(this, DeviceListActivity.class);
+        startActivity(intent);
+
     }
 }
---
+++ OpenTxmobile/app/src/main/java/com/choi_n_yang/opentx_mobile/opentx_mobile/SerialConsoleActivity.java
@@ -0,0 +1,203 @@
+package com.choi_n_yang.opentx_mobile.opentx_mobile;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
+import android.util.Log;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.choi_n_yang.opentx_mobile.opentx_mobile.usbserial.driver.UsbSerialPort;
+import com.choi_n_yang.opentx_mobile.opentx_mobile.usbserial.util.HexDump;
+import com.choi_n_yang.opentx_mobile.opentx_mobile.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SerialConsoleActivity extends Activity {
+
+    private final String TAG = SerialConsoleActivity.class.getSimpleName();
+
+    /**
+     * Driver instance, passed in statically via
+     * {@link #show(Context, UsbSerialPort)}.
+     *
+     * <p/>
+     * This is a devious hack; it'd be cleaner to re-create the driver using
+     * arguments passed in with the {@link #startActivity(Intent)} intent. We
+     * can get away with it because both activities will run in the same
+     * process, and this is a simple demo.
+     */
+    private static UsbSerialPort sPort = null;
+
+    private TextView mTitleTextView;
+    private TextView mDumpTextView;
+    private ScrollView mScrollView;
+    private CheckBox chkDTR;
+    private CheckBox chkRTS;
+
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+
+    private SerialInputOutputManager mSerialIoManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+                @Override
+                public void onRunError(Exception e) {
+                    Log.d(TAG, "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    SerialConsoleActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            SerialConsoleActivity.this.updateReceivedData(data);
+                        }
+                    });
+                }
+            };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_serial_console);
+        mTitleTextView = (TextView) findViewById(R.id.demoTitle);
+        mDumpTextView = (TextView) findViewById(R.id.consoleText);
+        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
+        chkDTR = (CheckBox) findViewById(R.id.checkBoxDTR);
+        chkRTS = (CheckBox) findViewById(R.id.checkBoxRTS);
+
+        chkDTR.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    sPort.setDTR(isChecked);
+                }catch (IOException x){}
+            }
+        });
+
+        chkRTS.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                try {
+                    sPort.setRTS(isChecked);
+                }catch (IOException x){}
+            }
+        });
+
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopIoManager();
+        if (sPort != null) {
+            try {
+                sPort.close();
+            } catch (IOException e) {
+                // Ignore.
+            }
+            sPort = null;
+        }
+        finish();
+    }
+
+    void showStatus(TextView theTextView, String theLabel, boolean theValue){
+        String msg = theLabel + ": " + (theValue ? "enabled" : "disabled") + "\n";
+        theTextView.append(msg);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Resumed, port=" + sPort);
+        if (sPort == null) {
+            mTitleTextView.setText("No serial device.");
+        } else {
+            final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+            UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
+            if (connection == null) {
+                mTitleTextView.setText("Opening device failed");
+                return;
+            }
+
+            try {
+                sPort.open(connection);
+                sPort.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+
+                showStatus(mDumpTextView, "CD  - Carrier Detect", sPort.getCD());
+                showStatus(mDumpTextView, "CTS - Clear To Send", sPort.getCTS());
+                showStatus(mDumpTextView, "DSR - Data Set Ready", sPort.getDSR());
+                showStatus(mDumpTextView, "DTR - Data Terminal Ready", sPort.getDTR());
+                showStatus(mDumpTextView, "DSR - Data Set Ready", sPort.getDSR());
+                showStatus(mDumpTextView, "RI  - Ring Indicator", sPort.getRI());
+                showStatus(mDumpTextView, "RTS - Request To Send", sPort.getRTS());
+
+            } catch (IOException e) {
+                Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
+                mTitleTextView.setText("Error opening device: " + e.getMessage());
+                try {
+                    sPort.close();
+                } catch (IOException e2) {
+                    // Ignore.
+                }
+                sPort = null;
+                return;
+            }
+            mTitleTextView.setText("Serial device: " + sPort.getClass().getSimpleName());
+        }
+        onDeviceStateChange();
+    }
+
+    private void stopIoManager() {
+        if (mSerialIoManager != null) {
+            Log.i(TAG, "Stopping io manager ..");
+            mSerialIoManager.stop();
+            mSerialIoManager = null;
+        }
+    }
+
+    private void startIoManager() {
+        if (sPort != null) {
+            Log.i(TAG, "Starting io manager ..");
+            mSerialIoManager = new SerialInputOutputManager(sPort, mListener);
+            mExecutor.submit(mSerialIoManager);
+        }
+    }
+
+    private void onDeviceStateChange() {
+        stopIoManager();
+        startIoManager();
+    }
+
+    private void updateReceivedData(byte[] data) {
+        final String message = "Read " + data.length + " bytes: \n"
+                + HexDump.dumpHexString(data) + "\n\n";
+        mDumpTextView.append(message);
+        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
+    }
+
+    /**
+     * Starts the activity, using the supplied driver instance.
+     *
+     * @param context
+     * @param driver
+     */
+    static void show(Context context, UsbSerialPort port) {
+        sPort = port;
+        final Intent intent = new Intent(context, SerialConsoleActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        context.startActivity(intent);
+    }
+
+}
