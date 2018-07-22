53ee05bca33c1628a46c1d8c8ac60b12a83b9efd
c1byte <kyuhae.choi@gmail.com>
2018-07-22 15:11:42 +0900

    Remove Devicelist code Connect anytime

--- OpenTxmobile/app/src/main/java/com/choi_n_yang/opentx_mobile/opentx_mobile/MainActivity.java
+++ OpenTxmobile/app/src/main/java/com/choi_n_yang/opentx_mobile/opentx_mobile/MainActivity.java
@@ -26,30 +26,38 @@ public class MainActivity extends AppCompatActivity {
     private UsbSerialPort port;
     public void  onClick(View view) {
 
-        Intent intent = new Intent(this, DeviceListActivity.class);
-        startActivity(intent);
+        this.updateConnectionStatus();
+        if (m_App.getUsbSerialPort() != null) {
+            port = m_App.getUsbSerialPort();
+            SerialConsoleActivity.show(this, port);
 
-        port = m_App.getUsbSerialPort();
-        SerialConsoleActivity.show(this, port);
+        }
 
     }
 
 
 
     public void onConnect(View view) {
-        TextView statusTextView;
-        statusTextView = (TextView) findViewById(R.id.txtStatusView);
 
         m_App.openUsbSerialPort();
+        this.updateConnectionStatus();
 
+
+    }
+    public void onDisConnect(View view){
+
+        m_App.closeUsbSerialPort();
+        this.updateConnectionStatus();
+    }
+
+    public void updateConnectionStatus(){
+        TextView statusTextView;
+        statusTextView = (TextView) findViewById(R.id.txtStatusView);
         if (m_App.getUsbSerialPort() == null) {
             statusTextView.setText("Serial device is not connected");
         } else {
             statusTextView.setText("Serial device: " + m_App.getUsbSerialPort().getClass().getSimpleName());
         }
     }
-    public void onDisConnect(View view){
 
-        m_App.closeUsbSerialPort();
-    }
 }
